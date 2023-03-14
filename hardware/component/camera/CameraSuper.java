697a8cbbc0bbd4ea14b988aba5cf5d684c929f2f
Dallin Guisti <dallin@guisti.org>
2023-03-13 20:36:44 -0700

    Update tile calculation and camera object

--- hardware/component/camera/CameraSuper.java
+++ hardware/component/camera/CameraSuper.java
@@ -7,12 +7,14 @@ import org.openftc.easyopencv.OpenCvCamera;
 
 import incognito.cog.hardware.component.HardwareComponent;
 import incognito.teamcode.robot.component.camera.cv.Pipeline;
+import org.openftc.easyopencv.OpenCvCameraRotation;
 
 public class CameraSuper extends HardwareComponent {
 
     // Instance Variables
     protected Pipeline pipeline;
     protected OpenCvCamera cvCamera;
+    protected boolean isStreaming = true;
 
     public CameraSuper(HardwareMap hardwareMap, Telemetry telemetry) {
         super(hardwareMap, telemetry);
@@ -27,8 +29,23 @@ public class CameraSuper extends HardwareComponent {
     /**
      * Terminate Camera stream to save resources.
      */
-    public void terminateCamera() {
+    public void stopCamera() {
+        isStreaming = false;
         cvCamera.stopStreaming();
         pipeline.stop();
+    }
+
+    public void startCamera() {
+        isStreaming = true;
+        cvCamera.startStreaming(320, 240, OpenCvCameraRotation.UPRIGHT);
+        pipeline.start();
+    }
+
+    public void toggleCamera() {
+        if (isStreaming) {
+            stopCamera();
+        } else {
+            startCamera();
+        }
     }
 }
--- hardware/component/drive/TileCalculation.java
+++ hardware/component/drive/TileCalculation.java
@@ -407,6 +407,8 @@ public class TileCalculation {
 
     public void finalizeTrajectory() {
         addTrajectory(finalizingTrajectory);
+        lastTrajectory = finalizingTrajectory;
+        finalizingTrajectory = null;
     }
 
     public void reset() {

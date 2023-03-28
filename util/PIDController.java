da1f47fa5dda314301cdf924b70d6fccefec04a7
Dallin Guisti <dallin@guisti.org>
2023-03-27 20:03:05 -0700

    Update PID controller

--- util/PIDController.java
+++ util/PIDController.java
@@ -1,30 +1,52 @@
 package incognito.cog.util;
 
 import com.qualcomm.robotcore.hardware.PIDCoefficients;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 public class PIDController {
     private PIDCoefficients pidCoefficients;
-    private double value;
     private double desired_value;
     private double previous_error, current_error = 0;
     private double P, I, D = 0;
+    ElapsedTime timer = new ElapsedTime();
 
 
     public PIDController(PIDCoefficients pidCoefficients) {
         this.pidCoefficients = pidCoefficients;
     }
 
-    public void update(double new_value) {
+    public double update(double new_value) {
         this.current_error = new_value - desired_value;
-        this.D = this.current_error - this.previous_error;
-        this.I += this.current_error;
+        this.D = (this.current_error - this.previous_error) / timer.seconds();
+        this.I += this.current_error * timer.seconds();
         this.P = this.current_error;
         this.previous_error = this.current_error;
-        this.value = new_value;
+        timer.reset();
+        return getOutput();
     }
 
     public double getOutput() {
-        return pidCoefficients.p * value + pidCoefficients.i * I + pidCoefficients.d * D;
+        return pidCoefficients.p * P + pidCoefficients.i * I + pidCoefficients.d * D;
+    }
+
+    public void setDesiredValue(double desired_value) {
+        this.desired_value = desired_value;
+    }
+
+    public void reset() {
+        this.previous_error = 0;
+        this.current_error = 0;
+        this.P = 0;
+        this.I = 0;
+        this.D = 0;
+    }
+
+    public void setPIDCoefficients(PIDCoefficients pidCoefficients) {
+        this.pidCoefficients = pidCoefficients;
+    }
+
+    public PIDCoefficients getPIDCoefficients() {
+        return pidCoefficients;
     }
 
 
